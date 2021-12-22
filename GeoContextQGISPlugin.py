8f74717da9aae4d59155bdf4885330ddd2efa30e
vermeulendivan <vermeulendivan@gmail.com>
2021-12-22 18:31:20 +0200

    Added and updated comments for all code/methods

--- GeoContextQGISPlugin.py
+++ GeoContextQGISPlugin.py
@@ -197,7 +197,8 @@ class GeoContextQGISPlugin:
             add_to_menu=True,
             add_to_toolbar=False)
 
-        self.point_tool.canvasClicked.connect(self.canvas_click)  # Enables the cursor tool
+        # Trigger for when the user clicks in the canvas when the panel is open and the cursor is active
+        self.point_tool.canvasClicked.connect(self.canvas_click)
 
     def onClosePlugin(self):
         """Cleanup necessary items here when plugin dockwidget is closed"""
@@ -278,6 +279,19 @@ class GeoContextQGISPlugin:
             pass
 
     def process_points_layer(self, dialog):
+        """
+        This method processes a point layer provided by the user.
+        The methods takes the point layer provided by the user, and then
+        requests the selected registry/key in the processing dialog.
+        A new file is created which stores the original attributes with the
+        newly requested data. The method will only process the selected features
+        if enabled by the user, and the layer can also be loaded into QGIS once
+        processing is done.
+
+        :param dialog: Contains the GeoContextQGISPlugin_processing object. It is required
+        by this method to retrieve the options selected by the user.
+        """
+
         input_points = dialog.get_input_layer()  # QgsVectorLayer. Input point layer from canvas
         selected_features = dialog.get_selected_option()  # Selected feature will only be taken into account if True
         registry = dialog.get_registry()  # Service, group or collection
@@ -287,19 +301,20 @@ class GeoContextQGISPlugin:
         load_output_file = dialog.get_layer_load_option()  # Loads the newly created file if True
 
         output_file_name = os.path.basename(output_file)
-        if selected_features and input_points.selectedFeatureCount() > 0:
-            if output_file.endswith(".gpkg"):
+        if selected_features and input_points.selectedFeatureCount() > 0:  # If the selection option is enabled and there is a selection
+            if output_file.endswith(".gpkg"):  # Geopackage format
                 QgsVectorFileWriter.writeAsVectorFormat(input_points, output_file, 'UTF-8', input_points.crs(), onlySelected=True)
-            elif output_file.endswith(".shp"):
+            elif output_file.endswith(".shp"):  # Shapefile format
                 QgsVectorFileWriter.writeAsVectorFormat(input_points, output_file, 'UTF-8', input_points.crs(), "ESRI Shapefile", onlySelected=True)  # shp format
             input_new = QgsVectorLayer(output_file, output_file_name)
         else:  # If the only selection option is disabled or there is no features selected
-            if output_file.endswith(".gpkg"):
+            if output_file.endswith(".gpkg"):  # Geopackage format
                 QgsVectorFileWriter.writeAsVectorFormat(input_points, output_file, 'UTF-8', input_points.crs())
-            elif output_file.endswith(".shp"):
+            elif output_file.endswith(".shp"):  # Shapefile format
                 QgsVectorFileWriter.writeAsVectorFormat(input_points, output_file, 'UTF-8', input_points.crs(), "ESRI Shapefile")  # shp format
             input_new = QgsVectorLayer(output_file, output_file_name)
 
+        # The user selected the 'Service' registry option
         if registry == 'Service':
             input_new.startEditing()
             new_field = QgsField(field_name, QVariant.String)
@@ -308,66 +323,73 @@ class GeoContextQGISPlugin:
             input_new.commitChanges()
 
             input_new.startEditing()
-            for input_feat in input_new.getFeatures():
+            for input_feat in input_new.getFeatures():  # Processes each of the features contained by the vector file
                 new_field_index = input_feat.fieldNameIndex(field_name)
 
                 feat_geom = input_feat.geometry()
-                if not feat_geom.isNull():
+                if not feat_geom.isNull():  # If a point does not contain geometry, it is skipped
                     point = feat_geom.asPoint()
                     x = point.x()
                     y = point.y()
 
+                    # The data is requested from the server
                     point_data = self.point_request_dialog(x, y, dialog)
                     point_value_str = str(point_data['value'])
 
                     input_new.changeAttributeValue(input_feat.id(), new_field_index, point_value_str)
             input_new.commitChanges()
+        # The user selected the 'Group' registry option
         elif registry == 'Group':
             print("group")
+        # The user selected the 'Collection' registry option
         elif registry == 'Collection':
             # Adds all of the fields to the layer
-            for input_feat in input_new.getFeatures():
+            for input_feat in input_new.getFeatures():  # Processes each of the features contained by the vector file
                 feat_geom = input_feat.geometry()
-                if not feat_geom.isNull():
+                if not feat_geom.isNull():  # If a point does not contain geometry, it is skipped
                     point = feat_geom.asPoint()
                     x = point.x()
                     y = point.y()
 
+                    # The data is requested from the server
                     point_data = self.point_request_dialog(x, y, dialog)
 
+                    # Each group contains a list of the 'Service' data associated with the group
                     list_dict_groups = point_data["groups"]
                     for dict_group in list_dict_groups:
                         list_dict_services = dict_group["services"]
-                        for dict_service in list_dict_services:
+                        for dict_service in list_dict_services:  # A field is added for each of the group service files
                             key = dict_service['key']
                             coll_field_name = field_name + key
 
+                            # Adds a new field to the attribute table
                             self.create_new_field(input_new, input_feat, coll_field_name)
                 break  # Fields only need to be added once for a layer
 
             # Requests values for all features
             for input_feat in input_new.getFeatures():
                 feat_geom = input_feat.geometry()
-                if not feat_geom.isNull():
+                if not feat_geom.isNull():  # If a point does not contain geometry, it is skipped
                     point = feat_geom.asPoint()
                     x = point.x()
                     y = point.y()
 
+                    # The data is requested from the server
                     point_data = self.point_request_dialog(x, y, dialog)
 
                     collection_name = point_data['name']
-                    list_dict_groups = point_data["groups"]
+                    list_dict_groups = point_data["groups"]  # Each group contains a list of the 'Service' data associated with the group
                     for dict_group in list_dict_groups:
                         group_name = dict_group['name']
 
-                        list_dict_services = dict_group["services"]
+                        list_dict_services = dict_group["services"]  # Service files for a group
                         for dict_service in list_dict_services:
                             key = dict_service['key']
                             point_value_str = dict_service['value']
                             coll_field_name = field_name + key
 
                             input_new.startEditing()
-                            field_index = input_feat.fieldNameIndex(coll_field_name)
+                            field_index = input_feat.fieldNameIndex(coll_field_name)  # Gets the index of the newly added field
                             input_new.startEditing()
                             input_new.changeAttributeValue(input_feat.id(), field_index, point_value_str)
                             input_new.commitChanges()
@@ -377,65 +399,128 @@ class GeoContextQGISPlugin:
             QgsProject.instance().addMapLayer(input_new)
 
     def point_request_panel(self, x, y):
+        """Return the value rettrieved from the ordered dictionary containing the requested data
+        from the server. This method is used by the docket widget panel of the plugin.
+
+        This method requests the data from the server for the given point coordinates.
+
+        :param x: Longitude coordinate
+        :type x: Numeric
+
+        :param y: Latitude coordinate
+        :type y: Numeric
+
+        :returns: The value retrieved for the request for the provided location
+        :rtype: Numeric
+        """
+
         settings = QgsSettings()
 
-        api_url = settings.value('geocontext-qgis-plugin/url')
-        registry = (self.dockwidget.cbRegistry.currentText()).lower()
-        key_name = self.dockwidget.cbKey.currentText()
+        api_url = settings.value('geocontext-qgis-plugin/url')  # Base URL. This will/should be set in  the options dialog
+        registry = (self.dockwidget.cbRegistry.currentText()).lower()  # Service, group or collection
+        key_name = self.dockwidget.cbKey.currentText()  # Key name, e.g. Elevation
 
-        dict_key = self.dockwidget.find_name_info(key_name)
+        dict_key = self.dockwidget.find_name_info(key_name)  # Retrieves the request key using the selected key name
         key = dict_key['key']
 
+        # Performs the request
         client = Client()
         url_request = api_url + "query?" + 'registry=' + registry + '&key=' + key + '&x=' + str(x) + '&y=' + str(y) + '&outformat=json'
-
         data = client.get(url_request)
+
         return data['value']
 
     def point_request_dialog(self, x, y, dialog):
+        """Return the value rettrieved from the ordered dictionary containing the requested data
+        from the server. This method is used by the processing dialog of the plugin.
+
+        This method requests the data from the server for the given point coordinates.
+
+        :param x: Longitude coordinate
+        :type x: Numeric
+
+        :param y: Latitude coordinate
+        :type y: Numeric
+
+        :param dialog: Stores the dialog which takes the required parameters as input
+        :type dialog: GeoContextQGISPlugin_processing_dialog
+
+        :returns: The data retrieved for the request for the provided location
+        :rtype: Ordered dictionary
+        """
+
         settings = QgsSettings()
 
-        api_url = settings.value('geocontext-qgis-plugin/url')
-        registry = dialog.get_registry()
-        key_name = dialog.get_key()
+        api_url = settings.value('geocontext-qgis-plugin/url')  # Base URL. Set in the options dialog
+        registry = dialog.get_registry()  # Gets the registry option selected by the user
+        key_name = dialog.get_key()  # Gets the key name set by the user
 
-        dict_key = dialog.find_name_info(key_name, registry)
+        dict_key = dialog.find_name_info(key_name, registry)  # Retrieved the key ID using the key name
         key = dict_key['key']
 
+        # Performs the request from the server based on the above information
         client = Client()
         url_request = api_url + "query?" + 'registry=' + registry.lower() + '&key=' + key + '&x=' + str(x) + '&y=' + str(y) + '&outformat=json'
-
         data = client.get(url_request)
 
         return data
 
     def canvas_click(self, point_tool):
-        x = point_tool[0]
-        y = point_tool[1]
+        """
+        This method is called when the plugin docket panel is open and the user clicks
+        in the canvas. The method will then request the selected data at the selected location.
 
+        :param dialog: Stores the dialog which takes the required parameters as input
+        :type dialog: GeoContextQGISPlugin_processing_dialog
+        """
+
+        # The coordinates from the QGIS canvas point tool
+        x = point_tool[0]  # Longitude
+        y = point_tool[1]  # Latitude
+
+        # Sets the panel values to the above
         self.dockwidget.lineLong.setText(str(x))
         self.dockwidget.lineLat.setText(str(y))
 
+        # Perfors a point data request from the server
         current_key_name = self.dockwidget.cbKey.currentText()
         data = self.point_request_panel(x, y)
 
         registry = self.dockwidget.cbRegistry.currentText()
 
+        # Service option
         if registry.lower() == 'service':
             settings = QgsSettings()
+
+            # If set in the options dialog, the table will automatically be cleared. This option is only available for Services
             auto_clear_table = settings.value('geocontext-qgis-plugin/auto_clear_table', False, type=bool)
             if auto_clear_table:
                 self.dockwidget.clear_results_table()
 
             self.dockwidget.tblResult.insertRow(0)  # Always add at the top of the table
-            self.dockwidget.tblResult.setItem(0, 0, QTableWidgetItem(current_key_name))
-            self.dockwidget.tblResult.setItem(0, 1, QTableWidgetItem(str(data)))
+            self.dockwidget.tblResult.setItem(0, 0, QTableWidgetItem(current_key_name))  # Sets the key in the table
+            self.dockwidget.tblResult.setItem(0, 1, QTableWidgetItem(str(data)))  # Sets the description
         elif registry.lower() == "group":  # UPDATE
             list_groups = []
         elif registry.lower() == "collection":  # UPDATE
             list_collections = []
 
     def create_new_field(self, input_layer, input_feat, field_name):
+        """Return index of the field in the input layer attribute table.
+
+        :param input_layer: Layer being processed.
+        :type input_layer: QgsVectorLayer
+
+        :param input_feat: Used to retrieve the field index
+        :type input_feat: QgsFeature
+
+        :param field_name: Field name which needs to be retrieved
+        :type field_name: String
+
+        :returns: The index of the field name, -1 if the attribute table does not contain the field
+        :rtype: Integer
+        """
+
         field_index = input_feat.fieldNameIndex(field_name)
         if field_index == -1:  # Checks if the field does not exist in the attribute table
             input_layer.startEditing()
